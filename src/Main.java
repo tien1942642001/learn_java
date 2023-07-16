ea5d724f20f84b49a9a9c6787afef8a4ebd29db7
tien1942642001 <tien194264@huce.edu.vn>
2023-07-16 17:22:27 +0700

    count string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("nhap 1: ");
+        String sourceStr = scan.nextLine();
+        System.out.print("nhap 2: ");
+        String searchStr = scan.nextLine();
+        int n = searchStr.length();
+        int count = 0;
+        for (int i = 0; i <= sourceStr.length() - n; i++) {
+            String x = sourceStr.substring(i, i+n);
+            if (x.equals(searchStr)) {
+                count++;
+            }
+        }
+        System.out.println("so lan: " + count);
     }
 }
